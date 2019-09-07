b30b6d4943bf4ddc539fffa81ee5ca325faaeef8
Simeon Akpanudo <akpanudosimeon@gmail.com>
2019-09-07 16:43:36 +0100

    Test coverage added to cover on format string type

--- test/useCFormat.test.js
+++ test/useCFormat.test.js
@@ -8,6 +8,22 @@ describe("Uses the C native format style to format dates into human readable str
 				"%A, %B %d, %Y %I:%M:%S %p"
 			)
 		).toBe("Tuesday, August 20, 2019 09:23:56 PM");
+
+		expect(
+			useCFormat(
+				new Date("2000 02 20 19:23:56"),
+				"%A, %B %d, %Y %I:%M:%S %p %t %n %p%p %pp"
+			)
+		).toBe("Sunday, February 20, 2000 07:23:56 PM %t %n %p%p %pp");
+
+		expect(
+			useCFormat(
+				new Date("2000 02 20 19:23:56"),
+				"%A, %B %do %I:%M:%S %p is in the %Qo Quarter of %Y and %B is the %mo Month"
+			)
+		).toBe(
+			"Sunday, February 20th 07:23:56 PM is in the 1st Quarter of 2000 and February is the 2nd Month"
+		);
 	});
 
 	test("should throw a TypeError('invalid date')", () => {
