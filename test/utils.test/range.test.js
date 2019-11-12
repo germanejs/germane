2a51afdff92426f2d1f7dcfbd66b0ebd5733d324
Simeon Akpanudo <akpandosimeon@hotmail.com>
2019-11-12 12:05:57 +0100

    moved each test files to the folder containing the files it is testing

--- test/utils.test/range.test.js
+++
@@ -1,5 +0,0 @@
-const range = require("../../lib/utils/range");
-
-test("should return a total sum of number that ranges from the current value to the end value including the step", () => {
-	expect(range(1, 10)).toBe(9);
-});
